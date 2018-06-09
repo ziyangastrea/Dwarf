53534f2deab457e6f159eece94b11966861483f5
ziyangastrea <ziyangastrea@gmail.com>
2018-06-09 19:31:14 +0800

    request and nightmare

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-console.log("One");
-console.log("Two");
-console.log("Three");
+var querystring = require('querystring');
+var shajs = require('sha.js')
+var request = require('request');
+var request = request.defaults({jar: true})
+//Debug用
+//require('request').debug = true
+
+var postData = {
+    userid: "rex721209",
+    passwd: shajs('sha256').update('movie1209').digest('hex'),
+    autologin : "y",
+    is_ajax: 1 
+}
+
+var formData = querystring.stringify(postData);
+var contentLength = formData.length;
+
+var options = {
+    method: 'POST',
+    url: 'https://www.sinsangmarket.kr/api/requestLogin.php',
+    headers: {
+        'Accept': '*/*',
+        'Content-Length': contentLength,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'www.sinsangmarket.kr',
+        'User-Agent': ''
+    },
+    body: formData
+};
+
+function callback(error, response, body) {
+    if (!error) {
+        console.log(body);
+        //轉成JSON用的
+        //var info = JSON.parse(JSON.stringify(body));
+        //console.log(info);
+    }
+    else {
+        console.log('Error happened: '+ error);
+    }
+}
+
+request(options, callback);
+
+request('http://www.sinsangmarket.kr/v2/goodsDetail?gid=15451250', function (error, response, body) {
+  console.log(body);
+});
---
+++ nightmare.js
@@ -0,0 +1,30 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
+/*
+nightmare
+  .goto('https://duckduckgo.com')
+  .type('#search_form_input_homepage', 'github nightmare')
+  .click('#search_button_homepage')
+  .wait('#r1-0 a.result__a')
+  .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+  .end()
+  .then(console.log)
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
+  */
+
+  nightmare
+  .goto('http://www.sinsangmarket.kr/v2/goodsDetail?gid=15451250')
+  .type('#userid', 'rex721209')
+  .type('#userpassword', 'movie1209')
+  .click('.loginButton')
+  .wait('.mainSwiper')
+  .goto('http://www.sinsangmarket.kr/v2/goodsDetail?gid=15451250')
+  //.evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+  //.end()
+  //.then(console.log)
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
